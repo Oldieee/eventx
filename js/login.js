f2000ff7ef9e71b208fc0b2512edea6e73f93be8
Oldieee <darius.ola@student.unitbv.ro>
2024-02-08 00:13:51 +0200

    updating the login backend

--- js/login.js
+++ js/login.js
@@ -9,11 +9,36 @@ class Login {
     let self = this;
     this.form.addEventListener('submit', (e) => {
       e.preventDefault();
+      var error = 0;
       self.fields.forEach((field) => {
         const input = document.querySelector(`#${field}`);
-        console.log(input.value);
+        if (self.validateFields(input) == false) {
+          error++;
+        }
+        if (error == 0) {
+          console.log('success');
+          localStorage.setItem('auth', 1);
+          this.form.submit();
+        }
       });
     });
+  }
+  validateFields(field) {
+    if (field.value.trim() == '') {
+      this.setStatus(
+        field,
+        `${field.previousElementSibling.innerText} cannot be blank`,
+        'error'
+      );
+      return false;
+    }
+  }
+  setStatus(field, message, status) {
+    const errorMessage = field.parentElement.querySelector('.error-message');
+    if (status == 'error') {
+      errorMessage.innerText = message;
+      field.classList.add('input-error');
+    }
   }
 }
 
